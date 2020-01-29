713b3c2e4f40707964f7c1c2ed369b27411eaea9
Eugênio Pierazzoli <pierazzoli@gmail.com>
2020-01-28 22:10:18 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+echo "Iniciando o Shell Script para monitoramento do processo phyton indicado no arquivo pid"
+
+while true
+	do
+	file="pid"
+	while IFS= read -r line
+		do
+		ps "$line" && echo "1: It is alive" || (echo "1: It is dead" &&  ./script.py  && sleep 4)
+		done < "file"
+
+	sleep 4
+done
+
